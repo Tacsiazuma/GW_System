7a4b0e7a516bdb6143cdb064219be66c7e22004f
Papp Krisztián <tacsiazuma@gmail.com>
2015-08-25 01:05:11 +0200

    Initial Commit

---
+++ ORM/Domain/ActiveRecord.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Papp
+ * Date: 2015.08.25.
+ * Time: 1:05
+ */
+
+namespace System\ORM\Domain;
+
+
+class ActiveRecord {
+
+}
---
+++ ORM/Domain/RowDataGateway.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Papp
+ * Date: 2015.08.25.
+ * Time: 1:04
+ */
+
+namespace System\ORM\Domain;
+
+
+class RowDataGateway {
+
+}
